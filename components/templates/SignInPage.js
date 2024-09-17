b15baf7de2fa5a94832e6261bd90792cd6094256
danial momen <danialmomenpour@gmail.com>
2024-09-17 14:42:41 +0330

    bug fixed-3

--- components/templates/SignInPage.js
+++ components/templates/SignInPage.js
@@ -38,7 +38,7 @@ function SignInPage() {
         type="text"
         placeholder="Email"
         value={email}
-        onChange={(e) => setEmail(e.target.value.trim())}
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <input
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,15 +3,17 @@ import { verifyPassword } from "@/utils/auth"
 import connectDB from "@/utils/connectDB"
 
 import NextAuth  from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const authOptions = {
-    session: {strategy: "jwt"} ,
+    session: { strategy: "jwt" },
     providers: [ CredentialsProvider({
 
         async authorize(credentials , req) {
 
             const {email , password} = credentials
+            console.log({email , password})
+
 
             try {
                 await connectDB()
@@ -27,6 +29,8 @@ const authOptions = {
 
             const isValid = await verifyPassword(password , user.password)
             if(!isValid) {throw new Error("username or password is incorrect")}
+
+            console.log("logined")
 
             return {email}
         }
